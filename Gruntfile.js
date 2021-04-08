436582b14c806f1b9bfb4625a9f485df5678f6a7
Mikey Saugstad <michaelssaugstad@gmail.com>
2021-04-07 20:11:57 -0700

    removes broken testing code

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,12 +51,6 @@ module.exports = function(grunt) {
                 dest: 'public/javascripts/FAQ/build/FAQ.js'
             }
         },
-        uglify: {
-            build: {
-                src: 'public/javascripts/SVLabel/build/SVLabel.js',
-                dest: 'public/javascripts/SVLabel/build/SVLabel.min.js'
-            }
-        },
         concat_css: {
             all: {
                 src: [
@@ -64,48 +58,6 @@ module.exports = function(grunt) {
                     'public/javascripts/SVLabel/css/*.css'
                     ],
                 dest: 'public/javascripts/SVLabel/build/SVLabel.css'
-            }
-        },
-        jasmine: {
-            src: [
-                'public/javascripts/SVLabel/src/SVLabel/canvas/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/data/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/game/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/keyboard/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/label/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/menu/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/mission/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/modal/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/navigation/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/neighborhood/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/onboarding/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/panorama/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/ribbon/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/status/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/task/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/user/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/util/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/zoom/*.js',
-                'public/javascripts/SVLabel/src/SVLabel/*.js'
-            ],
-            options: {
-                specs: [
-                    'public/javascripts/SVLabel/spec/SVLabel/*.js',
-                    'public/javascripts/SVLabel/spec/SVLabel/**/*.js'
-                ],
-                helpers: 'public/javascripts/SVLabel/spec/SpecHelper.js',
-                vendor: [
-                    'public/javascripts/SVLabel/lib/jquery-2.1.4.min.js',
-                    'public/javascripts/SVLabel/lib/d3.v3.js',
-                    'public/javascripts/SVLabel/lib/turf.min.js',
-                    'public/javascripts/SVLabel/lib/gsv/*.js',
-                    'public/javascripts/SVLabel/lib/kinetic-v4.3.3.min.js'
-                ]
-            }
-        },
-        jsdoc : {
-            dist : {
-                src: 'public/javascripts/SVLabel/src/SVLabel/*.js'
             }
         },
         watch : {
@@ -131,11 +83,7 @@ module.exports = function(grunt) {
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-concat-css');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
-
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['concat', 'concat_css']);
